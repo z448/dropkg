8f8019262ed04c89904a3d039387213b56e274ba
z448 <zed448@icloud.com>
2016-06-11 10:02:49 +0200

    - installs with -i to ~/local by default, or use -d ~/install/path

---
+++ test/env.sh
@@ -0,0 +1,4 @@
+alias cleanup='rm -rf control.tar.gz debian-binary data.tar.gz usr/';
+alias seebase="find ~/.dropkg && echo '^ contents of ~/.dropkg ^'";
+alias seelocal="find ~/local/usr && echo '^ contents of ~/local/usr ^'";
+alias seeconf="cat ~/.dropkg/.config.json && echo '^ ~/.dropkg/.config.json ^'"; 
